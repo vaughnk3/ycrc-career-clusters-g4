79fe4119e5a4a2143e1989a0b163d17ab4ffc2b8
andersong6 <andersong6@mailbox.winthrop.edu>
2024-04-05 23:18:26 -0400

    Documentation for all page components completed

--- career-clusters/src/components/page_Components/TopRectangle.js
+++ career-clusters/src/components/page_Components/TopRectangle.js
@@ -5,14 +5,17 @@ import './TopRectangle.css'
 /*
 This file contains the component consisting of an empty div used to style the top bar of some pages.
 
+Edits: (04/05/2024)
 KJ Vaughn
+Gavin T. Anderson
 */
+
 
 
 const TopRectangle = () => {
     return (
-        <div className = "top-rectangle" ></div>
+        <div className="top-rectangle"></div> /* Returns a div with the class 'top-rectangle' */
     )
 };
 
-export default TopRectangle;
+export default TopRectangle; /* Exports the TopRectangle component */
